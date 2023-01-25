4f9cb56cc90b98ac6532cfa584fad6f3550dc3cb
JesusCasanova <jesucasa@ucm.es>
2023-01-25 22:17:25 +0100

    JS changes

---
+++ validar.js
@@ -0,0 +1,11 @@
+function validar() {
+    var email, password, nombreUsuario, nombre;
+    email=document.getElementById("email").value;
+    password=document.getElementById("password").value;
+    nombreUsuario=document.getElementById("nombreUsuario").value;
+    nombre=document.getElementById("nombre").value;
+
+    expresion= /\w+@\w+\.+[a-z]/;
+
+    //TODO hacer switch con las condiciones.
+}
